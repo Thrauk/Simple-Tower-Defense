405c67675307b5263b5dc915b358cb6cbbf3da7c
Theodor Stoica <stoicath@gmail.com>
2024-02-12 21:58:38 +0200

    Added tile new assets, enemy movement and mouse events

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)]float speed = 1f;
     void Start()
     {
         StartCoroutine(FollowPath());
     }
 
-    IEnumerator FollowPath() {
-        foreach(Waypoint waypoint in path) {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+    IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+            // transform.position = waypoint.transform.position;
+            // yield return new WaitForSeconds(waitTime);
         }
     }
 
