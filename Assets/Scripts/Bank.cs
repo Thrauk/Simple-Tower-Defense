7ff9f6a8d3c698cb4c8009de06b90dc7d9ccb88e
Theodor Stoica <stoicath@gmail.com>
2024-02-14 15:53:23 +0200

    Gold balance UI and Refactoring

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
     
     [SerializeField] int currentBalance;
+
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     public int CurrentBalance { get { return currentBalance;}}
 
     private void Awake() {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount) {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount) {
+        UpdateDisplay();
         currentBalance -= Mathf.Abs(amount);
         if(currentBalance < 0) {
             // Lose the game
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay() {
+        displayBalance.text = $"Gold: {currentBalance}";
     }
 
     void ReloadScene() {
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
-    [SerializeField] int currentHitPoints = 0;
+    [Tooltip("Adds amount to maxHitPoints when enemy dies")]
+    [SerializeField] int difficultyRamp = 1;
+    int currentHitPoints = 0;
 
     Enemy enemy;
 
@@ -28,8 +31,8 @@ public class EnemyHealth : MonoBehaviour
     {
         currentHitPoints--;
         if(currentHitPoints <= 0) {
-            // Destroy(gameObject);
             gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
             enemy.RewardGold();
         }
     }
